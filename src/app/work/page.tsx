d95ba5b5eaee4dfdcf5afdf56af4c1f13591f0c2
Aryan <coolaryanbajpai20@gmail.com>
2024-02-25 12:40:17 +0530

    updated

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Card from "@/component/work/card";
 export default function Work(){
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1>Work Page</h1>
+            <Card/>
         </main>
     );
 }
