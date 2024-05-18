7e5bd0f317ff66b14a8a508b9e8ee611cabeb3e7
Aryan <coolaryanbajpai20@gmail.com>
2024-05-19 00:16:33 +0530

    Update

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -3,14 +3,7 @@ import { Inter } from 'next/font/google';
 import Card from "@/component/work/card";
 export default function Work() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 work">
-            <div className="background">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 work" style={{backgroundImage: "url(/images/dark-black-bg.jpg)", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center", top: "0", left: "0", right: "0", bottom: "0"}}>
             <Card />
         </main>
     );
--- src/component/common/navbar.tsx
+++ src/component/common/navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function App() {
   return (
-    <main className="flex z-10 background-transparent flex-col items-center justify-between" style={{ background: "transparent" }}>
+    <main className="flex z-10 flex-col items-center justify-between" style={{backgroundColor: "transparent",position: "absolute", top: "0", left: "0", right: "0", bottom: "0"}}>
       <nav className="nav">
         <Link href="/" className="sitename"><i className="fa-solid fa-house"></i></Link>
         <Link href="/about" className="menu">About</Link>
